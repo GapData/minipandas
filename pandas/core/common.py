ec52699f01abaab9495419e879d98aee814435e8
preich <preich@192.168.1.101>
2018-04-17 21:35:40 +0100

    remove commented code

--- pandas/core/common.py
+++ pandas/core/common.py
@@ -1,7 +1,3 @@
 def _dict_keys_to_ordered_list(mapping):
-    # when pandas drops support for Python < 3.6, this function
-    # can be replaced by a simple list(mapping.keys())
-    #if PY36 or isinstance(mapping, OrderedDict):
     keys = list(mapping.keys())
     return keys
-    #else:
